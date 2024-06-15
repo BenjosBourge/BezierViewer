641ba41cd8dcbe25a6d193badd95981d706272bb
Benjos <benjamin@bourge.be>
2024-06-15 16:34:06 +0200

    [+]Start of the project

---
+++ ClickableObject.gd
@@ -0,0 +1,26 @@
+extends StaticBody3D
+
+class_name Clickable
+
+var is_selected = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func is_clicked():
+	pass
+
+
+func can_be_selected():
+	return true
+
+
+func _on_movement(deplacment):
+	pass
